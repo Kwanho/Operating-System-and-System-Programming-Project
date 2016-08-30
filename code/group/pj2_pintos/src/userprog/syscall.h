78ff18ab5bcd10b5e4689648160a706ac67d3786
Kwanho Ryu <jack7249125@gmail.com>
2016-08-29 23:39:25 -0700

    final code

--- code/group/pj2_pintos/src/userprog/syscall.h
+++
@@ -1,9 +0,0 @@
-#ifndef USERPROG_SYSCALL_H
-#define USERPROG_SYSCALL_H
-
-void syscall_init (void);
-
-/* Added by Group 51 */
-void validate_mem (const void *uaddr);
-
-#endif /* userprog/syscall.h */
